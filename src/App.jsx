6a5e650476e75d8e63932478cb3f391972b8181d
red-x-silver <milkjazzy@gmail.com>
2024-07-08 23:20:40 +0100

    [xy]finally the font looks alright

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"></link>
     <Header />
     <Routes>  
       <Route path="/" element={<Home />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import{Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="bg-black shadow-md">
+    <header className="bg-black shadow-md font-family:roboto-mono">
         <div className='flex justify-between items-center p-4 '>
             <Link to='/'>
             <h1 className=' text-sm sm:text-2xl flex flex-wrap'>
@@ -13,13 +13,13 @@ export default function Header() {
             </h1>
             </Link>
 
-            <form className='bg-white p-3 rounded-lg flex items-center'>
+            <form className='bg-white p-3 rounded-lg flex items-center standard-text'>
             <input type='text' placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-80' />
             <FaSearch className='text-blue-600' />
             </form> 
 
             <nav>
-                <ul className='flex gap-4'>
+                <ul className='flex gap-4 standard-text'>
                 <Link to='/'> <li className='mx-2 text-white hidden sm:inline hover:underline'>Home</li> </Link>
                 <Link to='/projects'>   <li className='mx-2 text-white hidden sm:inline hover:underline'>Projects</li></Link>
                  <Link to='/about'>   <li className='mx-2 text-white hidden sm:inline hover:underline'>About</li></Link>
