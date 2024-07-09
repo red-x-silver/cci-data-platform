c36c900428bf47700ec807d91f2d71a2ea38edae
red-x-silver <milkjazzy@gmail.com>
2024-07-09 12:03:21 +0100

    [xy]created log in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup,login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/sign-up', signup);
+router.post('/log-in', login);
 
 export default router;
