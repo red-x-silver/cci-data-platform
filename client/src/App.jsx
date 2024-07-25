395bf893663418352ad8c4b885a5d2e31887df72
red-x-silver <milkjazzy@gmail.com>
2024-07-25 13:29:52 +0100

    [xy]added images slider to the dataset page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import UploadDataset from './pages/UploadDataset';
 import UpdateDataset from './pages/UpdateDataset';
+import Dataset from './pages/Dataset';
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
       </Route>
       
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dataset/:datasetId" element={<Dataset />} />
       <Route path="/log-in" element={<SignIn />} />
       <Route path="/About" element={<About />} />
       <Route path="/projects" element={<Projects />} />
--- client/src/pages/Dataset.jsx
+++ client/src/pages/Dataset.jsx
@@ -1,7 +1,155 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+//import Contact from '../components/Contact';
+
 
 export default function Dataset() {
+  SwiperCore.use([Navigation]);
+  const [dataset, setdataset] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchdataset = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/dataset/get/${params.datasetId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setdataset(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchdataset();
+  }, [params.datasetId]); //[para,s.datasetID] is the dependecy of useEffect()
+
   return (
-    <div>Dataset</div>
-  )
+    <main>
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+
+      {error && (
+        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+      )}
+
+      {dataset && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {dataset.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className='h-[550px]'
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+              </SwiperSlide>
+                ))}
+          </Swiper>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
+
+          {/* <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+            <p className='text-2xl font-semibold'>
+              {dataset.name} - ${' '}
+              {dataset.offer
+                ? dataset.discountPrice.toLocaleString('en-US')
+                : dataset.regularPrice.toLocaleString('en-US')}
+              {dataset.type === 'rent' && ' / month'}
+            </p>
+            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+              <FaMapMarkerAlt className='text-green-700' />
+              {dataset.address}
+            </p>
+            <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                {dataset.type === 'rent' ? 'For Rent' : 'For Sale'}
+              </p>
+              {dataset.offer && (
+                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                  ${+dataset.regularPrice - +dataset.discountPrice} OFF
+                </p>
+              )}
+            </div>
+            <p className='text-slate-800'>
+              <span className='font-semibold text-black'>Description - </span>
+              {dataset.description}
+            </p>
+            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBed className='text-lg' />
+                {dataset.bedrooms > 1
+                  ? `${dataset.bedrooms} beds `
+                  : `${dataset.bedrooms} bed `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBath className='text-lg' />
+                {dataset.bathrooms > 1
+                  ? `${dataset.bathrooms} baths `
+                  : `${dataset.bathrooms} bath `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaParking className='text-lg' />
+                {dataset.parking ? 'Parking spot' : 'No Parking'}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaChair className='text-lg' />
+                {dataset.furnished ? 'Furnished' : 'Unfurnished'}
+              </li>
+            </ul>
+            {currentUser && dataset.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact dataset={dataset} />}
+          </div> */}
+        </div>
+      )}
+      </main>
+  );
 }
